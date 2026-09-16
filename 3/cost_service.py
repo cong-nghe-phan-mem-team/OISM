4bd24a6c9fc7c93daf4f045ac2a9c242461c72b3
nganlephuong96-art <nganlephuong96@gmail.com>
2026-09-16 11:17:29 +0700

    Delete 3 directory

--- 3/cost_service.py
+++
@@ -1,6 +0,0 @@
-from app.repositories.cost_repo import CostRepository
-from sqlalchemy.orm import Session
-class CostService:
-    @staticmethod
-    def confirm_purchase(db:Session,receipt_id:int,tenant_id:int):
-        return CostRepository.confirm_receipt(db,receipt_id,tenant_id)
